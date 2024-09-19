9ca6b6838992f105f5f8fb230b65024b4239c2b0
Namia7 <144546746+Namia7@users.noreply.github.com>
2024-09-19 21:04:31 +0200

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,8 @@
+const buttonRed = document.querySelector('.red-button') 
+const buttonBlack = document.querySelector('.black-button') 
+const buttonGreen = document.querySelector('.green-button')
+const buttonBlue = document.querySelector('.blue-button')
+const buttonOrange = document.querySelector('.orange-button')
+const buttonPink = document.querySelector('.pink-button')
+const body = document.body
+
---
+++ main.js
@@ -0,0 +1,25 @@
+buttonRed.addEventListener('click', () => {
+    changeColor('red');
+});
+
+buttonBlack.addEventListener('click', () => {
+    changeColor('black');
+});
+
+buttonGreen.addEventListener('click', ()=>{
+    changeColor('green')
+})
+
+buttonBlue.addEventListener('click', ()=>{
+    changeColor('blue')
+})
+
+buttonOrange.addEventListener('click', ()=>{
+    changeColor('orange')
+})
+
+buttonPink.addEventListener('click', ()=>{
+    changeColor('pink')
+})
+
+
